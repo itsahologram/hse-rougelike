7b571423d3392b63bdccce5dd1ace22087775bd1
Tatiana Shimanskya <ttnsimanskaa@gmail.com>
2023-03-18 18:45:43 +0300

    add first SFML project

---
+++ map.hpp
@@ -0,0 +1,24 @@
+#ifndef TEST_SFML_MAP_HPP
+#define TEST_SFML_MAP_HPP
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+const int H_MAP = 11;
+const int W_MAP = 18;
+const sf::String TileMap[H_MAP] = {
+        "111111111111111111",
+        "100000000000000001",
+        "100000000000000001",
+        "100000000000000001",
+        "111111111000000001",
+        "000000001000000001",
+        "000000001000000001",
+        "111111111000000001",
+        "100000000000000001",
+        "100000000000000001",
+        "111111111111111111"
+
+};
+
+#endif //TEST_SFML_MAP_HPP
